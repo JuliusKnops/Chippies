3657019867de609572b23bca818db724ca7f7567
JuliusKnops <juliusknops@hotmail.com>
2023-01-13 15:12:48 +0100

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ class Netlist():
     def __init__(self, netlist_sourcefile, print_sourcefile):
         self.gates = self.load_gates(print_sourcefile)
         self.load_connections(netlist_sourcefile)
-        #print(self.gates)
 
     def load_gates(self, sourcefile):
         gates = {}
@@ -13,29 +12,15 @@ class Netlist():
             reader = csv.DictReader(in_file) 
 
             for row in reader:
-                chip_name = row['chip']
-                chip_x = row['x']
-                chip_y = row['y']
-                #print(chip_name, chip_x, chip_y)
-                gates[row['chip']] = Gates(chip_x, chip_y, chip_name)
+                gates[row['chip']] = Gates(row['x'], row['y'], row['chip'])
+
         return gates
     
     def load_connections(self, sourcefile):
         with open(sourcefile) as in_file:
             reader = csv.DictReader(in_file) 
             for row in reader:
-                print(f"row = {row}")
-                print(self.gates)
-                print(f"row chip_a = {row['chip_a']}")
-                print(self.gates[row['chip_a']])
                 self.gates[row['chip_a']].add_connections(self.gates[row['chip_b']])  
-                ##if row[1] == str(l):
-                 #   print("ja")
-            """for row in reader:
-                print(f"row = {row}")
-                for l in self.gates:
-                    print(l)"""
-
 
 class Gates():
     def __init__(self, x_coordinate, y_coordinate, name):
@@ -46,7 +31,6 @@ class Gates():
     
     def add_connections(self, NewPoint):
         self.connections.add(NewPoint)
-
 
 if __name__ == "__main__":
     chip_nr = 0 # loopt van 0 tot en met 2
@@ -62,33 +46,3 @@ if __name__ == "__main__":
     for chip in netlist.gates:
         print(chip, netlist.gates[chip].x, netlist.gates[chip].y, netlist.gates[chip].connections)
     
-
-
-
-"""    with open(netlist_file) as in_file:
-        reader = csv.DictReader(in_file) 
-
-        for row in reader:
-            print(row)
-    
-    with open(print_file) as in_file:
-        reader = csv.DictReader(in_file) 
-
-        for row in reader:
-            chip_name = row['chip']
-            chip_x = row['x']
-            chip_y = row['y']
-            print(chip_name, chip_x, chip_y)"""
-    
-
-
-"""
-import csv
-def load_nodes(self, source_file):
-    nodes = {}
-
-    with open(self, source_file) as in_file:
-        reader = csv.csv.DictReader(in_file) 
-
-        for row in reader:
-"""
