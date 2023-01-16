3d22f4a4ab652df684581293060f9c42da96f72f
JuliusKnops <juliusknops@hotmail.com>
2023-01-16 11:40:16 +0100

    main

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class Gates():
     def __init__(self, x_coordinate, y_coordinate, name):
         self.x = x_coordinate
         self.y = y_coordinate
-        self.name = name
+        self.name = name  
         self.connections = set()
     
     def add_connections(self, NewPoint):
