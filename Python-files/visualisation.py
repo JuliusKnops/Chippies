54db99a6c7522d7f82dc365e409c99b97e94c73b
11677112 <hiddeb@xs4all.nl>
2023-01-17 11:25:40 +0100

    Test visualisation.py that uses data files instead of hardcoding

--- Python-files/visualisation.py
+++ Python-files/visualisation.py
@@ -1,44 +1,47 @@
-
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d 
+import ast
 
+with open("output.csv", 'r') as p:
+    pathways = csv.reader(p)
+    next(pathways)
+    path_list = []
+    for paths in pathways:
+        path_list.append(ast.literal_eval(paths[1]))
+    path_list.pop()
 
+with open("Print_0.csv", 'r') as g:
+    gates = csv.reader(g)
+    next(gates)
+    x_gate = []
+    y_gate = []
+    for gate in gates:
+        x_gate.append(ast.literal_eval(gate[1]))
+        y_gate.append(ast.literal_eval(gate[2]))
 
-
-#Matplotlib hardcode suboptimal example chip 0
+#Plot 3d grid with dimensions 0 to 7 
 fig = plt.figure()
-
 ax = fig.add_subplot(projection= "3d")
 ax.set_xlim(0, 7); ax.set_ylim(0, 7); ax.set_zlim(0, 7)
-x = [1,6,4,6,3]
-y = [5,5,4,2,1]
-z = [0,0,0,0,0]
 
-ax.scatter(x, y, z, c='red', s=100)
-x_unit = [1, 2, 3, 4, 5, 6]
-y_unit = [5, 5, 5, 5, 5, 5]
-z_unit = [0, 0, 0, 0, 0, 0]
-ax.plot(x_unit, y_unit, z_unit, c='blue')
 
-x_unit = [1, 1, 2, 3, 4]
-y_unit = [5, 4, 4, 4, 4]
-z_unit = [0, 0, 0, 0, 0]
-ax.plot(x_unit, y_unit, z_unit, c='green')
+# place gates 
+ax.scatter(x_gate, y_gate, c='red', s=100)
 
-x_unit = [4, 4, 3, 2, 1, 0, 0, 0, 0, 1, 2, 3, 3]
-y_unit = [4, 3, 3, 3, 3, 3, 2, 1, 0, 0, 0, 0, 1]
-z_unit = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-ax.plot(x_unit, y_unit, z_unit, c='yellow')
-
-x_unit = [6, 5, 5, 5, 6, 6]
-y_unit = [2, 2, 3, 4, 4, 5]
-z_unit = [0, 0, 0, 0, 0, 0]
-ax.plot(x_unit, y_unit, z_unit, c='purple')
-
-x_unit = [3, 4, 5, 6, 7, 7, 6]
-y_unit = [1, 1, 1, 1, 1, 2, 2]
-z_unit = [0, 0, 0, 0, 0, 0, 0]
-ax.plot(x_unit, y_unit, z_unit, c='purple')
-
+# Print each pathway
+for i in path_list:
+    x_unit = []
+    y_unit = []
+    z_unit = []
+    if path_list[2] == None:
+        path_list[2] == 0
+    for j in i:
+        x_unit.append(j[0])
+        y_unit.append(j[1])
+        z_unit.append(0)
+    ax.plot(x_unit, y_unit, z_unit)
 plt.show()
+
+
