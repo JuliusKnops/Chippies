a7d16ef998d791e88d9cc4ede81f9ce9f7d38161
Deniz Mermer <deniz.mermer@student.uva.nl>
2023-01-22 21:47:49 +0100

    moved from main.py

--- code/algorithms/random_algo.py
+++ code/algorithms/random_algo.py
@@ -1,54 +1,8 @@
 import csv
 import random
-#from code.algorithms.Astar import *
 import time
-
-class Netlist():
-    def __init__(self, netlist_sourcefile, print_sourcefile):
-        self.gates = self.load_gates(print_sourcefile)
-        self.load_connections(netlist_sourcefile)
-
-        #########################################################
-        ### Insert gate locations, connections, nodes and n, k cost
-        #########################################################
-        self.used_connections = set()
-        self.used_nodes = set()
-        self.gate_locations = set([(int(g.x), int(g.y), g.z) for g in self.gates.values()])
-
-        self.k = 0
-        self.n = 0
-
-        ###############################
-        ### End
-        ###############################
-
-    def load_gates(self, sourcefile):
-        gates = {}
-
-        with open(sourcefile) as in_file:
-            reader = csv.DictReader(in_file) 
-
-            for row in reader:
-                gates[row['chip']] = Gates(row['x'], row['y'], row['chip'])
-
-        return gates
-    
-    def load_connections(self, sourcefile):
-        with open(sourcefile) as in_file:
-            reader = csv.DictReader(in_file) 
-            for row in reader:
-                self.gates[row['chip_a']].add_connections(self.gates[row['chip_b']])
-
-class Gates():
-    def __init__(self, x_coordinate, y_coordinate, name):
-        self.x = int(x_coordinate)
-        self.y = int(y_coordinate)
-        self.z = 0
-        self.name = name  
-        self.connections = set()
-    
-    def add_connections(self, NewPoint):
-        self.connections.add(NewPoint)
+from code.classes.netlist import *
+from code.classes.gates import *
 
 def random_algo(netlist):
 
