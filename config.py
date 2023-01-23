dad54dda298687041e3f229116f2fc6d39308cae
Deniz Mermer <deniz.mermer@student.uva.nl>
2023-01-23 15:27:33 +0100

    removed unused global vars

--- config.py
+++ config.py
@@ -4,11 +4,8 @@
 
 from code.classes import netlist
 
-grid_rows = 3
-grid_cols = 3
-grid_layers = 1
 chip_nr = 0 # loopt van 0 tot en met 2
-netlist_nr = 1 # loopt van 1 tot en met 3
+netlist_nr = 2 # loopt van 1 tot en met 3
 netlist_file = f"data/chip_{chip_nr}/netlist_{netlist_nr + 3 * chip_nr}.csv"
 print_file = f"data/chip_{chip_nr}/print_{chip_nr}.csv"
 Astar_netlist = netlist.Netlist(netlist_file, print_file)
