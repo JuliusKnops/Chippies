c12920ac7e759a7291d403794801442a9e1b3f42
11677112 <hiddeb@xs4all.nl>
2023-01-26 20:12:48 +0100

    Visualisation

--- code/visualisation/visualisation.py
+++ code/visualisation/visualisation.py
@@ -3,7 +3,13 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d 
 
 def visualisation(solution, gates, chip_nr):
+    """function for visualising chips
 
+    Args:
+        solution (list): list of all pathways
+        gates (list): list with coördinates of gates
+        chip_nr (int): number of chip to indicate dimensions
+    """    
     #Plot 3d grid with dimensions 0 to 7 
     fig = plt.figure()
     ax = fig.add_subplot(projection= "3d")
@@ -13,6 +19,8 @@ def visualisation(solution, gates, chip_nr):
         ax.set_xlim(0, 17); ax.set_ylim(0, 17); ax.set_zlim(0, 7)
     else:
         ax.set_xlim(0, 17); ax.set_ylim(0, 17); ax.set_zlim(0, 7)
+    # add labels 
+    ax.set_xlabel("X-axis"); ax.set_ylabel("Y-axis"); ax.set_zlabel("Z-axis")
 
     # place gates 
     x_gates = []
@@ -22,6 +30,7 @@ def visualisation(solution, gates, chip_nr):
         y_gates.append(i[1])
         ax.scatter(x_gates, y_gates, c='red', s=25)
 
+    # append coördinates in right list for the pathways
     for i in solution:
         x_unit = []
         y_unit = []
@@ -31,5 +40,6 @@ def visualisation(solution, gates, chip_nr):
             y_unit.append(j[1])
             z_unit.append(j[2])
         ax.plot(x_unit, y_unit, z_unit)
+    # plot figure
     plt.show()
 
