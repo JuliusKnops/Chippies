fd4a6a04fd5aba73c86efe7bda0780146fc8e3e9
Deniz Mermer <deniz.mermer@student.uva.nl>
2023-02-01 20:51:45 +0100

    finalization and typing

--- code/algorithms/dijkstra.py
+++ code/algorithms/dijkstra.py
@@ -1,13 +1,21 @@
 from . import Astar
 
 class Node_dijkstra(Astar.Node_Astar):
-    def __init__(self, x, y, z, cost = 0, parent = None):
+    def __init__(self, x: int, y: int, z: int, cost = 0, parent = None) -> None:
         super().__init__(x, y, z, cost, parent)
 
-    # No estimation
     def __lt__(self, other):
+        """
+        Finds the cheapest node only by using path cost and no heuristics.
+        """
         return self.cost < other.cost
 
 class PathFinder_dijkstra(Astar.PathFinder_Astar):
-    def __init__(self, Start_chip, End_chip, auto_pathfind=True, Node = Node_dijkstra):
+    """
+    This class is the same as Astar with the difference being that dijkstra
+    does not use an estimation heuristics g(x).
+    """
+    def __init__(self, Start_chip: object, End_chip:object,
+                auto_pathfind=True,
+                Node = Node_dijkstra) -> None:
         super().__init__(Start_chip, End_chip, auto_pathfind=True, Node = Node)
